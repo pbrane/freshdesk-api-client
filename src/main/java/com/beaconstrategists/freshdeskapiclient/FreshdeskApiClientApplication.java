67014588923b35e3d13bdde9e75e70a785276ca5
David Hustace <david@beaconstrategists.com>
2024-12-14 09:23:17 -0500

    Depend on TAC API Jackson ObjectMapper Configuration

--- src/main/java/com/beaconstrategists/freshdeskapiclient/FreshdeskApiClientApplication.java
+++ src/main/java/com/beaconstrategists/freshdeskapiclient/FreshdeskApiClientApplication.java
@@ -1,11 +1,12 @@
 package com.beaconstrategists.freshdeskapiclient;
 
-import com.beaconstrategists.freshdeskapiclient.config.JacksonConfig;
+import com.beaconstrategists.taccaseapiservice.config.api.JacksonConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(JacksonConfig.class)
 public class FreshdeskApiClientApplication {
 
     public static void main(String[] args) {
